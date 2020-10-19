12fdabdf700b173198f673670f4616fffeebb10f
John ZuHone <jzuhone@gmail.com>
2020-10-19 14:40:25 -0400

    Refactor tests to match new signature

--- fep1_actel_check/tests/test_fep1actel_sql.py
+++ fep1_actel_check/tests/test_fep1actel_sql.py
@@ -3,19 +3,22 @@ from acis_thermal_check.regression_testing import \
     RegressionTester, all_loads
 import pytest
 
-fep1actel_rt = RegressionTester(FEP1ActelCheck, model_path, "fep1_actel_test_spec.json")
-
-# SQL state builder tests
-
-fep1actel_rt.run_models(state_builder='sql')
+@pytest.fixture(autouse=True, scope='module')
+def fa_rt(test_root):
+    # SQL state builder tests
+    rt = RegressionTester(FEP1ActelCheck, model_path,
+                          "fep1_actel_test_spec.json",
+                          test_root=test_root, sub_dir='sql')
+    rt.run_models(state_builder='sql')
+    return rt
 
 # Prediction tests
 
 
 @pytest.mark.parametrize('load', all_loads)
-def test_prediction(answer_store, load):
+def test_prediction(fa_rt, answer_store, load):
     if not answer_store:
-        fep1actel_rt.run_test("prediction", load)
+        fa_rt.run_test("prediction", load)
     else:
         pass
 
@@ -23,8 +26,8 @@ def test_prediction(answer_store, load):
 
 
 @pytest.mark.parametrize('load', all_loads)
-def test_validation(answer_store, load):
+def test_validation(fa_rt, answer_store, load):
     if not answer_store:
-        fep1actel_rt.run_test("validation", load)
+        fa_rt.run_test("validation", load)
     else:
         pass
